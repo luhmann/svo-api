47bb731c7e1ad98e7d14a8d30d0e3a9051eb7012
Jan Florian Dietrich <jfd@daenen4.de>
2016-02-15 00:09:46 +0100

    chore(setup): add feathers-hooks

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 // @flow
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import crypto from 'crypto'
 import feathers from 'feathers'
+import hooks from 'feathers-hooks'
 import mongodb from 'feathers-mongodb'
 
 const app = feathers()
+const prefix = 'api/v1'
+
 // enable cors
 app.use(cors())
 
@@ -14,6 +18,8 @@ let recipeService = mongodb({
   collection: 'recipes'
 })
 
+app.configure(hooks())
+
 // Setup a rest interface
 app.configure(feathers.rest())
 
@@ -22,7 +28,15 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // Use the service object
-app.use('/api/v1/recipe', recipeService)
+app.use(`${prefix}/recipe/:slug`, recipeService)
+
+// app
+//   .service(`${prefix}/recipe/:slug`)
+//   .before({
+//     create (hook, next) {
+//       hook.data.createdAt = new Date()
+//     }
+//   })
 
 // Start the application on port 3030
 app.listen(3030)
