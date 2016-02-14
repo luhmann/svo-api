24387bc4bbf06b30d4428b37b4df47cad56bfa35
Jan Florian Dietrich <jfd@daenen4.de>
2016-02-14 16:57:07 +0100

    chore(setup): enable cors, enable versioning

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 // @flow
+import bodyParser from 'body-parser'
+import cors from 'cors'
 import feathers from 'feathers'
 import mongodb from 'feathers-mongodb'
-import bodyParser from 'body-parser'
 
 const app = feathers()
+// enable cors
+app.use(cors())
 
 let recipeService = mongodb({
   host: '127.0.0.1',
@@ -19,7 +22,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // Use the service object
-app.use('/recipe', recipeService)
+app.use('/api/v1/recipe', recipeService)
 
 // Start the application on port 3030
 app.listen(3030)
