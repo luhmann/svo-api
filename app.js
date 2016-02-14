13ed1ddccf7025a0413a772b63681218747a00a5
Jan Florian Dietrich <jfd@daenen4.de>
2016-02-14 16:32:26 +0100

    chore(setup): initial setup

---
+++ app.js
@@ -0,0 +1,25 @@
+// @flow
+import feathers from 'feathers'
+import mongodb from 'feathers-mongodb'
+import bodyParser from 'body-parser'
+
+const app = feathers()
+
+let recipeService = mongodb({
+  host: '127.0.0.1',
+  db: 'svo',
+  collection: 'recipes'
+})
+
+// Setup a rest interface
+app.configure(feathers.rest())
+
+// Parse JSON and form HTTP bodies
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// Use the service object
+app.use('/recipe', recipeService)
+
+// Start the application on port 3030
+app.listen(3030)
