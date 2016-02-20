ec1517665f38dc136455e55fe057c2fe415ec46c
Jan Florian Dietrich <jfd@daenen4.de>
2016-02-20 14:22:39 +0100

    refactor: switch to feathers2

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import feathers from 'feathers'
 import hooks from 'feathers-hooks'
+import { isArray } from 'lodash'
 import mongoose from 'mongoose'
 import mongooseService from 'feathers-mongoose'
+import rest from 'feathers-rest'
 import uniqueSlug from 'unique-slug'
 
 import Recipe from './model/RecipeModel.js'
@@ -25,7 +27,7 @@ mongoose.connect('mongodb://localhost:27017/svo', (err, connection) => {
 app.configure(hooks())
 
 // Setup a rest interface
-app.configure(feathers.rest())
+app.configure(rest())
 
 // Parse JSON and form HTTP bodies
 app.use(bodyParser.json())
@@ -33,7 +35,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 // Use the service object
 app.use(`${prefix}/recipe`, mongooseService({
-  name: 'recipe',
   Model: Recipe
 }))
 
@@ -44,13 +45,30 @@ app.use((error, req, res, next) => {
 app
   .service(`${prefix}/recipe`)
   .before({
-    create: (hook, next) => {
+    create (hook) {
       hook.data.hash = uniqueSlug(hook.data.slug)
-      next()
+    },
+    get (hook) {
+      if (hook.id) {
+        return this.find({ hash: uniqueSlug(hook.id) })
+          .then(data => {
+            if (isArray(data) && data.length === 1 && data[0].slug === hook.id) {
+              let result = data[0].toObject({ versionKey: false })
+              delete result['_id']
+              hook.result = result
+              return hook
+            }
+          })
+      }
     }
   })
+  // .after({
+  //   get: (hook, next) => {
+  //     // console.log(hook.result)
+  //   }
+  // })
 
 // Start the application on port 3030
 app.listen(3030, () => {
-  console.log('App is now listening on port 3030')
+  console.log('SVO API is now listening on port 3030')
 })
