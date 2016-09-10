ee5a299fd3eae998465a97c5b7750aa623daf40e
Jan Florian Dietrich <jfd@daenen4.de>
2016-09-10 08:34:43 +0200

    feat(debug): Bind to 0.0.0.0 to allow more universal use

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import app from './app.js'
 
 // Start the application on port 3030
 try {
-  app.listen(3030, () => {
+  app.listen(3030, '0.0.0.0', () => {
     console.log('SVO API is now listening on port 3030')
   })
 } catch (e) {
